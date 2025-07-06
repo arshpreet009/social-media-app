78e6637e19605107b5569ef88e8a538ced81b943
Arshpreet Singh <preetarsh2021@gmail.com>
2025-07-06 18:44:28 +0530

    Moved base URL to .env file

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:4000' });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
 
 export const logIn = (formData) => API.post('/auth/login', formData); 
 
 export const signUp = (formData) => API.post('/auth/register', formData);
+
+;
--- client/src/api/UploadRequest.js
+++ client/src/api/UploadRequest.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:4000' });
+const API = axios.create({
+  baseURL: process.env.REACT_APP_BASE_URL,
+});
+
 
 export const uploadImage = (data) => API.post('/upload/', data);
 export const uploadPost = (data) => API.post('/post', data);
